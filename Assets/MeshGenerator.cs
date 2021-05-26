2f547ca300de70c66f3e0541e2f8030025bcf078
sanderbuist <sybuist@gmail.com>
2021-05-26 11:47:03 +0200

    started on mesh script for terrain gen

---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
